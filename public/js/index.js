13eb0c670397391f124c493933b5c227255baf67
DoWake <298666015@qq.com>
2023-09-15 00:05:20 +0800

    新增头像交互效果

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
 const canvasEl = document.getElementById("bg");
 StarrySky.init(canvasEl);
 StarrySky.render();
+StarrySky.setStarSpeedLevel(0.0005);
+//鼠标移入，粒子加速
+const avatar = document.querySelector('.avatar');
+avatar.addEventListener('mouseover', function () {
+  StarrySky.setStarSpeedLevel(0.008);
+});
+//鼠标移出，粒子恢复
+avatar.addEventListener('mouseout', function () {
+  StarrySky.setStarSpeedLevel(0.0005);
+});
